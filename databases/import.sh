cd502315f7e3afddf0cdb218eab80de3fc756ef1
杜耀坤 <duyaokun@carystudio.com>
2015-06-16 14:35:24 +0800

    修正导入数据路径

--- databases/import.sh
+++ databases/import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #import databases to postgresql
 psql_user="gitlab"
-psql_path="/opt/gitlab/embedding/bin/"
+psql_path="/opt/gitlab/embedded/bin/"
 table_name=$1
 
 $ sudo -u ${psql_user} ${psql_path}psql -d gitlabhq_production -f ${table_name}.psql
