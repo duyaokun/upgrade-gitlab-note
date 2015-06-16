b45fac7a0a566b63a570bf074848c0cf63acfe32
杜耀坤 <duyaokun@carystudio.com>
2015-06-16 13:46:21 +0800

    在导出数据之前进行备份

--- databases/upgrade.sh
+++ databases/upgrade.sh
@@ -15,13 +15,19 @@ else
   echo "Start export database from MySQL"
   local mysql_password="6645a79e86"
 
+  # backup
+  /opt/gitlab-6.9.2-1/mysql/bin/mysqldump -u bitnami -p${mysql_password} bitnami_gitlab > ../bitnami_gitlab.sql
+
+  # prepared for export
   /opt/gitlab-6.9.2-1/mysql/bin/mysql -p${mysql_password} bitnami_gitlab < before-export.sql
 
+  # export
   for table in ${tables[@]}
   do
     ../export.sh ${table} ${mysql_password}
   done
 
+  # revert SQL changed
   /opt/gitlab-6.9.2-1/mysql/bin/mysql -p${mysql_password} bitnami_gitlab < after-export.sql
 
   echo "start tar mysql files"
