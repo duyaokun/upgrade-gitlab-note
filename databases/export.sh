ff77fda0278bf3ce160a48b154639df0fbc4fce1
杜耀坤 <duyaokun@carystudio.com>
2015-06-16 15:07:54 +0800

    导出数据时列出列名

--- databases/export.sh
+++ databases/export.sh
@@ -4,7 +4,7 @@ table=$1
 
 #导出 MySQL表格
 echo "start export table ${table}"
-/opt/gitlab-6.9.2-1/mysql/bin/mysqldump --no-create-db --no-create-info --compatible=postgresql --default-character-set=utf8 -r ${table}.sql -u bitnami bitnami_gitlab ${table} -p${mysql_password}
+/opt/gitlab-6.9.2-1/mysql/bin/mysqldump --complete-insert --no-create-db --no-create-info --compatible=postgresql --default-character-set=utf8 -r ${table}.sql -u bitnami bitnami_gitlab ${table} -p${mysql_password}
 
 #转换成 PostgreSQL表格
 echo "start convert table ${table}"
